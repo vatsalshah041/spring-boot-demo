a4f9a7f5a8bf93e9e3b828c49e9d86ad4d3ad699
Vatsal Shah <113013039+vatsalshah041@users.noreply.github.com>
2025-01-01 22:20:55 +0530

    first commit spring boot

---
+++ demo/src/main/java/com/example/demo/Myclass.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Myclass {
+
+    @GetMapping("")
+    public String sayHello()
+    {
+        return "Hello";
+    }
+
+}
