87b253e6979759ec8c2ad61046aa44d7491cb27f
Vatsal Shah <113013039+vatsalshah041@users.noreply.github.com>
2025-01-08 15:51:08 +0530

    Rest api demo done

--- demo/src/main/java/com/example/demo/Myclass.java
+++ demo/src/main/java/com/example/demo/Myclass.java
@@ -1,11 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Myclass {
 
+    //whenever we want to use another class over here
+    //it is dependency injection
+    //myclass is depended on dog as it uses dog over here
+    //more precisely it is called field injection
+    @Autowired
+    private Dog dog;
+
+    @GetMapping("/fun")
+    public String havingFun()
+    {
+        return dog.fun();
+    }
     @GetMapping("")
     public String sayHello()
     {
--- demo/src/main/java/com/example/demo/controller/EntryController.java
+++ demo/src/main/java/com/example/demo/controller/EntryController.java
@@ -1,7 +1,43 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.Entry;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 @RestController
+@RequestMapping("/journal")//adds mapping to the whole class
 public class EntryController {
+
+    private Map<Long,Entry> entries=new HashMap();
+    //path over here would be journal/trial since i used request mapping
+    @GetMapping
+    public List<Entry> getAll(){
+        return new ArrayList<>(entries.values());
+    }
+
+    @PostMapping
+    public String createEntry(@RequestBody Entry myEntry)
+    {
+        entries.put(myEntry.getId(),myEntry);
+        return "true";
+    }
+    @GetMapping("id/{myId}")
+    public Entry getById(@PathVariable  Long myId){
+        return entries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public boolean deletebyId(@PathVariable Long myId){
+        entries.remove(myId);
+        return true;
+    }
+
+    @PutMapping("id/{myid}")
+    public Entry updatebyId(@PathVariable Long myid,@RequestBody Entry myentry){
+        entries.put(myid,myentry);
+        return entries.get(myid);
+    }
 }
