3ed5fc2056a0207643c5c932f1acd056ea05c277
Vatsal Shah <113013039+vatsalshah041@users.noreply.github.com>
2025-01-09 19:27:10 +0530

    reposnse entity executed with some errors due to try ctach

--- demo/src/main/java/com/example/demo/controller/EntryControllerv2.java
+++ demo/src/main/java/com/example/demo/controller/EntryControllerv2.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Entry;
 import com.example.demo.entity.EntryV2;
 import com.example.demo.service.EntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/journalv2")//adds mapping to the whole class
@@ -17,34 +19,61 @@ public class EntryControllerv2 {
     private EntryService entryService;
 
     @GetMapping
-    public List<EntryV2> getAll(){
-        return entryService.getEntry();
+    public ResponseEntity<List<EntryV2>> getAll(){
+
+        List<EntryV2> entries= entryService.getEntry();
+        if(!entries.isEmpty()){
+            return new ResponseEntity<>(entries, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping
-    public boolean createEntry(@RequestBody EntryV2 myEntry) {
-        entryService.saveEntry(myEntry);
-        return true;
+    public ResponseEntity<?> createEntry(@RequestBody EntryV2 myEntry) {
+        try {
+            entryService.saveEntry(myEntry);
+            return new ResponseEntity<>(true,HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(false,HttpStatus.BAD_REQUEST);
+        }
     }
     @GetMapping("id/{myId}")
-    public EntryV2 getById(@PathVariable ObjectId myId){
-        return entryService.getEntryById(myId).orElse(null);
+    public ResponseEntity<EntryV2> getById(@PathVariable ObjectId myId){
+        Optional<EntryV2> findEntry= entryService.getEntryById(myId);
+        if(findEntry.isPresent()){
+            return new ResponseEntity<>(findEntry.get(),HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
     @DeleteMapping("id/{myId}")
-    public boolean deletebyId(@PathVariable ObjectId myId){
-         entryService.deleteById(myId);
-         return true;
+    public ResponseEntity<?> deletebyId(@PathVariable ObjectId myId){
+        try {
+            entryService.deleteById(myId);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("id/{myid}")
-    public EntryV2 updatebyId(@PathVariable ObjectId myid,@RequestBody EntryV2 newentry){
-        EntryV2 old=entryService.getEntryById(myid).orElse(null);
-        if(old!=null){
-            old.setTitle(newentry.getTitle()!=null && !newentry.getTitle().equals("")?newentry.getTitle():old.getTitle());
-            old.setContent(newentry.getContent()!=null && !newentry.getContent().equals("")? newentry.getContent() : old.getContent());
+    public ResponseEntity<EntryV2> updatebyId(@PathVariable ObjectId myid,@RequestBody EntryV2 newentry){
+        Optional<EntryV2> old=entryService.getEntryById(myid);
+        if(old.isPresent()){
+            old.get().setTitle(newentry.getTitle()!=null && !newentry.getTitle().equals("")?newentry.getTitle(): old.get().getTitle());
+            old.get().setContent(newentry.getContent()!=null && !newentry.getContent().equals("")? newentry.getContent() : old.get().getContent());
+            entryService.saveEntry(old.orElse(null));
+            return new ResponseEntity<>(old.get(),HttpStatus.OK);
         }
-        entryService.saveEntry(old);
-        return newentry;
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
