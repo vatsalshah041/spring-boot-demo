f791a6c9d93f5fdb5a242ad65459e7e308fe21e7
Vatsal Shah <113013039+vatsalshah041@users.noreply.github.com>
2025-01-10 13:50:28 +0530

    project lombok integrated for automatic getter setters

--- demo/src/main/java/com/example/demo/entity/Entry.java
+++ demo/src/main/java/com/example/demo/entity/Entry.java
@@ -1,34 +1,17 @@
 package com.example.demo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Entry {
     private long id;
     
     private String content;
 
     private String title;
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
 
 }
--- demo/src/main/java/com/example/demo/entity/EntryV2.java
+++ demo/src/main/java/com/example/demo/entity/EntryV2.java
@@ -1,42 +1,25 @@
 package com.example.demo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Getter
+@Setter
 @Document
 public class EntryV2 {
     @Id
     private ObjectId id;
-
-    public ObjectId getId() {
-        return id;
-    }
 
     private String content;
 
     private String title;
 
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
 
 }
